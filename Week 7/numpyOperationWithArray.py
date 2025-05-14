9cdd45c704340eecb01688347722a7c773aa931f
punit-2205 <punitjaiswal785621@gmail.com>
2025-05-14 23:25:08 +0530

    Add files via upload

---
+++ Week 7/numpyOperationWithArray.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+a = np.array([[1, 2], [4, 5]])
+b = np.array([[1, 2], [4, 5]])
+
+while True:
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Dot product")
+    print("6. Exponentiation")
+    print("7. Logarithm")
+    print("8. Natural logarithm")
+    print("9. Exit")
+    
+    n = int(input("Enter the option number: "))
+    
+    if 1 <= n <= 8:
+        if n == 1:
+            c = np.add(a, b)
+            print("Sum\n", c, "\n")
+        elif n == 2:
+            d = np.subtract(a, b)
+            print("Difference\n", d, "\n")
+        elif n == 3:
+            e = np.multiply(a, b)
+            print("Product\n", e, "\n")
+        elif n == 4:
+            f = np.divide(a, b)
+            print("Quotient\n", f, "\n")
+        elif n == 5:
+            g = np.dot(a, b)
+            print("Dot product\n", g, "\n")
+        elif n == 6:
+            h = np.exp(a)
+            i = np.exp(b)
+            print("Exponentiation for array a:\n", h)
+            print("Exponentiation for array b:\n", i, "\n")
+        elif n == 7:
+            l = np.log(a)
+            m = np.log(b)
+            print("Natural Log (ln) for array a:\n", l)
+            print("Natural Log (ln) for array b:\n", m, "\n")
+        elif n == 8:
+            x = np.log10(a)
+            y = np.log10(b)
+            print("Log base 10 for array a:\n", x)
+            print("Log base 10 for array b:\n", y, "\n")
+    elif n == 9:
+        break
+    else:
+        print("No such option exists, please enter a valid option.\n")
